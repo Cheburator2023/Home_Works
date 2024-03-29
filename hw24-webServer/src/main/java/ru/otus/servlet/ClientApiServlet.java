0528438b8eb54fd3402f7a81148df8a2331c09dc
Victor Gabbasov <borisborisov03102001@gmail.com>
2024-03-30 00:34:23 +0300

    hw-24-webServer-admin работает

--- hw24-webServer/src/main/java/ru/otus/servlet/ClientApiServlet.java
+++ hw24-webServer/src/main/java/ru/otus/servlet/ClientApiServlet.java
@@ -2,18 +2,21 @@ package ru.otus.servlet;
 
 import com.google.gson.Gson;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import ru.otus.crm.model.Client;
 import ru.otus.crm.service.DBServiceClient;
+import ru.otus.model.User;
 
 import java.io.IOException;
 
 @WebServlet("/api/client/*")
 public class ClientApiServlet extends HttpServlet {
 
+    private static final int ID_PATH_PARAM_POSITION = 1;
     private final DBServiceClient dbServiceClient;
     private final Gson gson;
 
@@ -48,5 +51,20 @@ public class ClientApiServlet extends HttpServlet {
         Client updatedClient = dbServiceClient.saveClient(clientToUpdate);
         resp.setContentType("application/json;charset=UTF-8");
         resp.getWriter().print(gson.toJson(updatedClient));
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Client client = dbServiceClient.getClient(extractIdFromRequest(request)).orElse(null);
+
+        response.setContentType("application/json;charset=UTF-8");
+        ServletOutputStream out = response.getOutputStream();
+        out.print(gson.toJson(client));
+    }
+
+    private long extractIdFromRequest(HttpServletRequest request) {
+        String[] path = request.getPathInfo().split("/");
+        String id = (path.length > 1) ? path[ID_PATH_PARAM_POSITION] : String.valueOf(-1);
+        return Long.parseLong(id);
     }
 }
