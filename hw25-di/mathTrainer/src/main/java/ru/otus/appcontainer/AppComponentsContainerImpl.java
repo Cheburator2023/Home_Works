7f074a0469f9a9d44fe67548a1dc4bfd32064738
Victor Gabbasov <borisborisov03102001@gmail.com>
2024-04-01 11:54:20 +0300

    hw-25-di-working with comments

--- hw25-di/mathTrainer/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ hw25-di/mathTrainer/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -6,6 +6,7 @@ import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 
 import java.lang.reflect.Method;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @SuppressWarnings("squid:S1068")
 public class AppComponentsContainerImpl implements AppComponentsContainer {
@@ -28,6 +29,9 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
         for (Method method : componentMethods) {
             AppComponent appComponent = method.getAnnotation(AppComponent.class);
+            if (appComponentsByName.containsKey(appComponent.name())) {
+                throw new IllegalArgumentException("Component with name '" + appComponent.name() + "' already exists!");
+            }
             Object component = createComponent(configInstance, method);
             appComponents.add(component);
             appComponentsByName.put(appComponent.name(), component);
@@ -69,12 +73,17 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
     @SuppressWarnings("unchecked")
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
-        for (Object component : appComponents) {
-            if (componentClass.isAssignableFrom(component.getClass())) {
-                return (C) component;
-            }
+        List<C> components = appComponents.stream()
+                .filter(componentClass::isInstance)
+                .map(componentClass::cast)
+                .collect(Collectors.toList());
+        if (components.isEmpty()) {
+            throw new RuntimeException("Component not found for: " + componentClass.getName());
         }
-        throw new RuntimeException("Component not found for: " + componentClass.getName());
+        if (components.size() > 1) {
+            throw new RuntimeException("More than one component found for: " + componentClass.getName());
+        }
+        return components.get(0);
     }
 
     @SuppressWarnings("unchecked")
