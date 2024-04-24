37b20c329cdd969cb2a25080cbb7dfee88ac9a92
Victor Gabbasov <borisborisov03102001@gmail.com>
2024-04-24 15:29:27 +0300

    hw-31-executors-корректный вывод в консоль

---
+++ hw31-executors/src/main/java/ru/otus/AlternatePrinting.java
@@ -0,0 +1,57 @@
+package ru.otus;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class AlternatePrinting {
+
+    private final ReentrantLock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition();
+    private boolean threadOneTurn = true;
+
+    public void printNumbers() {
+        Thread thread1 = new Thread(() -> print(1), "Поток 1");
+        Thread thread2 = new Thread(() -> print(2), "Поток 2");
+
+        thread1.start();
+        thread2.start();
+    }
+
+    private void print(int threadNumber) {
+        for (int i = 1; i <= 10; i++) {
+            lock.lock();
+            try {
+                while ((threadNumber == 1 && !threadOneTurn) || (threadNumber == 2 && threadOneTurn)) {
+                    condition.await();
+                }
+                System.out.println(Thread.currentThread().getName() + ":" + i);
+                threadOneTurn = !threadOneTurn;
+                condition.signalAll();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        for (int i = 10; i >= 1; i--) {
+            lock.lock();
+            try {
+                while ((threadNumber == 1 && !threadOneTurn) || (threadNumber == 2 && threadOneTurn)) {
+                    condition.await();
+                }
+                System.out.println(Thread.currentThread().getName() + ":" + i);
+                threadOneTurn = !threadOneTurn;
+                condition.signalAll();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new AlternatePrinting().printNumbers();
+    }
+}
