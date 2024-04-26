cd2cb883b061d32b358d0862514495d10cc431c0
Victor Gabbasov <borisborisov03102001@gmail.com>
2024-04-26 11:18:04 +0300

    hw-31-executors-убрал дублирование кода

--- hw31-executors/src/main/java/ru/otus/AlternatePrinting.java
+++ hw31-executors/src/main/java/ru/otus/AlternatePrinting.java
@@ -10,38 +10,27 @@ public class AlternatePrinting {
     private boolean threadOneTurn = true;
 
     public void printNumbers() {
-        Thread thread1 = new Thread(() -> print(1), "Поток 1");
-        Thread thread2 = new Thread(() -> print(2), "Поток 2");
+        Thread thread1 = new Thread(() -> {
+            printSequence(1, 1, 10);
+            printSequence(1, 9, 1);
+        }, "Поток 1");
+        Thread thread2 = new Thread(() -> {
+            printSequence(2, 1, 10);
+            printSequence(2, 9, 1);
+        }, "Поток 2");
 
         thread1.start();
         thread2.start();
     }
 
-    private void print(int threadNumber) {
-        for (int i = 1; i <= 10; i++) {
+    private void printSequence(int threadNumber, int start, int end) {
+        int step = start < end ? 1 : -1;
+        for (int i = start; step > 0 ? i <= end : i >= end; i += step) {
             lock.lock();
             try {
                 while ((threadNumber == 1 && !threadOneTurn) || (threadNumber == 2 && threadOneTurn)) {
                     condition.await();
                 }
-                Thread.sleep(1000);
-                System.out.println(Thread.currentThread().getName() + ":" + i);
-                threadOneTurn = !threadOneTurn;
-                condition.signalAll();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            } finally {
-                lock.unlock();
-            }
-        }
-
-        for (int i = 9; i >= 1; i--) {
-            lock.lock();
-            try {
-                while ((threadNumber == 1 && !threadOneTurn) || (threadNumber == 2 && threadOneTurn)) {
-                    condition.await();
-                }
-                Thread.sleep(1000);
                 System.out.println(Thread.currentThread().getName() + ":" + i);
                 threadOneTurn = !threadOneTurn;
                 condition.signalAll();
