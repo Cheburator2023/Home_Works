29722b2862464b173ce20a18aa559a344aa793a7
Victor Gabbasov <borisborisov03102001@gmail.com>
2024-05-07 11:33:47 +0300

    hw-32-concurrentCollections-work with comments

--- hw32-concurrentCollections/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
+++ hw32-concurrentCollections/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -8,39 +8,43 @@ import ru.otus.lib.SensorDataBufferedWriter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
 
 @Slf4j
 public class SensorDataProcessorBuffered implements SensorDataProcessor {
     private final int bufferSize;
     private final SensorDataBufferedWriter writer;
-    private final List<SensorData> buffer = new ArrayList<>();
+    private final ArrayBlockingQueue<SensorData> buffer;
 
     public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
         this.bufferSize = bufferSize;
         this.writer = writer;
+        this.buffer = new ArrayBlockingQueue<>(bufferSize);
     }
 
     @Override
     public void process(SensorData data) {
-        synchronized (buffer) {
-            buffer.add(data);
-            buffer.sort(Comparator.comparing(SensorData::getMeasurementTime));
-            if (buffer.size() >= bufferSize) {
-                flush();
+        if (!buffer.offer(data)) {
+            flush();
+            boolean added = buffer.offer(data);
+            if (!added) {
+                log.error("Не удалось добавить данные датчика после flush: {}", data);
             }
         }
     }
 
     public void flush() {
-        synchronized (buffer) {
-            try {
-                if (!buffer.isEmpty()) {
-                    writer.writeBufferedData(new ArrayList<>(buffer));
-                    buffer.clear();
-                }
-            } catch (Exception e) {
-                log.error("Ошибка в процессе записи буфера", e);
+        try {
+            List<SensorData> dataToWrite = new ArrayList<>();
+            buffer.drainTo(dataToWrite);
+
+            dataToWrite.sort(Comparator.comparing(SensorData::getMeasurementTime));
+
+            if (!dataToWrite.isEmpty()) {
+                writer.writeBufferedData(dataToWrite);
             }
+        } catch (Exception e) {
+            log.error("Ошибка в процессе записи буфера", e);
         }
     }
 
